f7e3722007deb08e851fa32016600fc2feef9175
Emilyguerrero <emilyguerrero59@gmail.com>
2023-08-17 12:05:30 -0400

    feat: modify the seed file to load the data using the Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+require 'faker'
+
+5.times do
+  Character.create(
+    name: Faker::Movies::HarryPotter.character,
+    location: Faker::Movies::HarryPotter.location,
+    house: Faker::Movies::HarryPotter.house
+  )
+end
